7c1717f0052be94e05324d64a8e88d0aa822eeb4
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-03-19 18:26:10 +0700

    finish

--- app/src/main/java/com/example/progressbarandseekbar25012021/MainActivity.java
+++ app/src/main/java/com/example/progressbarandseekbar25012021/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.progressbarandseekbar25012021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView mCount;
+    SeekBar mSk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCount = findViewById(R.id.textViewCount);
+        mSk = findViewById(R.id.seekBar1);
+
+        mSk.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @SuppressLint("DefaultLocale")
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                mCount.setText(String.format("Count : %d",i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
